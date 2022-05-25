fd47a2482f3df013d22d46393c2c8032a087f8d5
wellflat <wellflat@gmail.com>
2022-05-25 11:34:42 +0900

    added svelte sample

---
+++ svelte/my-svelte-project/src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+	export let name;
+	let count = 0;
+	function handleClick() {
+		count++;
+	}
+
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<button on:click={handleClick}>
+		clicked {count} {count === 1 ? 'time' : 'times'}
+	</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
